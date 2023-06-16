0f034610cddd63348b8371fd62d0843396c17b7e
flydog98 <guinnessoverflow@gmail.com>
2023-06-16 15:25:19 +0900

    feat: run ec2 command sample

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+import boto3
+import botocore
+import paramiko
 
 app = Flask(__name__)
 
@@ -9,6 +12,22 @@ def home():
 @app.route('/result')
 def result():
   # map-reduce 검색
+
+  key = paramiko.RSAKey.from_private_key_file("./config/dsc.pem")
+  client = paramiko.SSHClient()
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+  try:
+    client.connect(hostname="ec2-15-165-39-26.ap-northeast-2.compute.amazonaws.com", username="ubuntu", pkey=key)
+
+    stdin, stdout, stderr = client.exec_command("docker exec namenode1 ls")
+
+    print(stdout.read())
+
+    client.close()
+
+  except Exception as e:
+    print(e)
   
   return render_template('result.html')
 
