237e64a4ac7beea05b79217994b328bd19bd57e9
flydog98 <guinnessoverflow@gmail.com>
2023-06-19 15:49:18 +0900

    feat: removing unnecessary imports

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 from flask import request
 import time as t
-import boto3
-import botocore
 import paramiko
 
 app = Flask(__name__)
+
+category_dict = {"cafe":0, "restraunt":1, "shopping":2}
 
 @app.route('/')
 def home():
@@ -25,10 +25,10 @@ def result():
 
     print("request map-reduce")
     time = request.args.get('time')
-    category = request.args.get('category')
-    stdin, stdout, stderr = client.exec_command(f"hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -mapper 'python3 mapper.py' -file /home/hadoop/mapper.py -reducer 'python3 reducer.py {time}' -file /home/hadoop/reducer.py -input /input/ -output /output")
+    category = category_dict[request.args.get('category')]
+    stdin, stdout, stderr = client.exec_command(f"hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -mapper 'python3 mapper.py' -file /home/hadoop/mapper.py -reducer 'python3 reducer.py {time} {category}' -file /home/hadoop/reducer.py -input /input/ -output /output")
 
-    t.sleep(7)
+    t.sleep(30)
 
     print("reading file")
     stdin, stdout, stderr = client.exec_command("hdfs dfs -cat /output/*")
@@ -37,34 +37,25 @@ def result():
     print("removing file")
     stdin, _, stderr = client.exec_command("hdfs dfs -rm -R /output")
 
-    # stdin, _, stderr = client.exec_command("docker exec namenode1 /home/hadoop-3.3.0/bin/hdfs dfs -rm -R /output")
-
     client.close()
 
   except Exception as e:
     print(e)
-  
-  # stdout = b'place1,6 30\nplace2,1 123\nplace99,12  999\nplace44322,1 3'
 
   lines = list()
 
   for input in result.splitlines():
-    # print("input:", input)
     comma_pos = input.find(",")
     tab_pos = input.find("\t")
     dummy = list()
     dummy.append(input[:comma_pos])
     dummy.append(input[comma_pos + 1:comma_pos + 3])
     dummy.append(input[tab_pos + 1:])
-    # print("dummy:", dummy)
     lines.append(dummy)
 
   lines.sort(key = lambda x: -int(x[2]))
-
-  # print(lines)
   
   return render_template('result.html', result = lines[:5])
-  # return render_template('result.html')
 
 if __name__ == '__main__':
   app.run(port="1234", debug=True)
